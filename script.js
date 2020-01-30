47aed518f58214c590501b81a79bc0477663d67e
Eesha000 <summerkiel08@gmail.com>
2020-01-30 11:12:34 +0800

    adding event

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 $(document).ready(function () {
   $surface = $('.surface');
   $car = $('.car');
+
+  //keypress event 
+  $(document).on('keypress', function (e) {
+
+    if (e.which == 13) {
+      $($surface).toggleClass("moveRight");
+      $($car).toggleClass("suspension");
+    }
+  })
 })
